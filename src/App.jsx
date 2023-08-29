82fc2165056419a263fa271145f1f429e2479850
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-08-29 23:52:23 +0700

    Finishing Design and Coloring

--- src/App.jsx
+++ src/App.jsx
@@ -46,41 +46,48 @@ function App() {
   }, [arrayFilters])
 
   return (
-    <div className="min-h-screen py-6 px-8">
-      <div className="mx-auto max-w-5xl flex flex-wrap gap-4 bg-base-100 shadow-lg px-12 py-6 mb-4">
-        {arrayFilters.map((filter, index) => (
-          <span key={index} className="inline-flex items-center rounded-md text-sm font-medium bg-red-100 text-gray-800 overflow-hidden">
-            <span className="py-1 px-2"> {filter.value}</span>
-            <button type="button" onClick={() => removeFilter(filter)} className="flex-shrink-0 bg-blue-600 w-7 h-7 p-1">
-              <svg className="text-gray-500" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-                <path d="M6 18L18 6M6 6l12 12"></path>
-              </svg>
-            </button>
-          </span>
-
-        ))}
+    <>
+      <header className="w-full px-8 h-24 bg-[url('/images/bg-header-mobile.svg')] sm:bg-[url('/images/bg-header-desktop.svg')] bg-[#5ba4a4]" >
+        <div className="relative h-full mx-auto px-8 max-w-5xl ">
+          {arrayFilters.length > 0 &&
+            <div className="absolute inset-x-0 -bottom-8 rounded-md bg-white mx-auto max-w-5xl flex flex-wrap gap-4 bg-base-100 shadow-lg px-8 py-6">
+              {arrayFilters.map((filter, index) => (
+                <span key={index} className="inline-flex items-center rounded-md text-sm font-medium bg-[#eef6f6] text-[#5ba4a4] overflow-hidden">
+                  <span className="py-1 px-2"> {filter.value}</span>
+                  <button type="button" onClick={() => removeFilter(filter)} className="flex-shrink-0 bg-[#5ba4a4] w-7 h-7 p-1">
+                    <svg className="text-white font-bold" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                      <path d="M6 18L18 6M6 6l12 12"></path>
+                    </svg>
+                  </button>
+                </span>
+              ))}
+            </div>
+          }
+        </div>
+      </header>
+      <div className="min-h-screen py-12 px-8">
+        <div className={`mx-auto max-w-5xl ${arrayFilters.length > 0 && "mt-14 sm:mt-6"}`}>
+          {Data.filter(matchesFilters).map((data) => (
+            <Card
+              key={data.id}
+              logo={data.logo}
+              company={data.company}
+              newpost={data.new}
+              featured={data.featured}
+              position={data.position}
+              postedAt={data.postedAt}
+              contract={data.contract}
+              location={data.location}
+              role={data.role}
+              level={data.level}
+              languages={data.languages.map(language => language)}
+              tools={data.tools.map(tool => tool)}
+              filterFunction={handleFilterButton}
+            />
+          ))}
+        </div>
       </div>
-      <div className="mx-auto max-w-5xl">
-        {Data.filter(matchesFilters).map((data) => (
-          <Card
-            key={data.id}
-            logo={data.logo}
-            company={data.company}
-            newpost={data.new}
-            featured={data.featured}
-            position={data.position}
-            postedAt={data.postedAt}
-            contract={data.contract}
-            location={data.location}
-            role={data.role}
-            level={data.level}
-            languages={data.languages.map(language => language)}
-            tools={data.tools.map(tool => tool)}
-            filterFunction={handleFilterButton}
-          />
-        ))}
-      </div>
-    </div>
+    </>
   )
 }
 
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -16,17 +16,17 @@ const Card = (props) => {
     } = props
 
     return (
-        <div className={`flex justify-start items-start flex-col sm:flex-row sm:items-center gap-4 rounded py-4 px-8 mb-12 sm:mb-6 shadow-lg relative ${featured && "border-l-4"}`}>
+        <div className={`bg-white flex justify-start items-start flex-col sm:flex-row sm:items-center gap-4 rounded-md py-4 px-8 mb-12 sm:mb-6 shadow-lg relative ${featured && "border-l-4 border-[#5ba4a4]"}`}>
             <img src={logo} alt={company} className="rounded-full h-14 sm:h-16 absolute -top-[25px] sm:static" />
             <div className="flex flex-col justify-between items-start gap-2 mt-6 sm:mt-0">
                 <div className="flex justify-between items-center gap-2">
-                    <h3 className="text-sm font-bold">{company}</h3>
+                    <h3 className="text-sm font-bold text-[#5ba4a4]">{company}</h3>
                     <div className="font-extrabold text-xs gap-2">
-                        {newpost && <span className="bg-red-600 text-[0.60rem] leading-3 py-1 px-2 rounded-full mr-2">NEW!</span>}
-                        {featured && <span className="bg-red-600 text-[0.60rem] leading-3 py-1 px-2 rounded-full mr-2">FEATURED</span>}
+                        {newpost && <span className="bg-[#5ba4a4] text-[0.60rem] text-white leading-3 py-1 px-2 rounded-full mr-2">NEW!</span>}
+                        {featured && <span className="bg-[#2c3a3a] text-[0.60rem] text-white leading-3 py-1 px-2 rounded-full mr-2">FEATURED</span>}
                     </div>
                 </div>
-                <h4 className="text-base font-bold">{position}</h4>
+                <h4 className="text-base font-bold text-[#2c3a3a]">{position}</h4>
                 <div className="flex justify-between items-center gap-4 text-xs font-semibold">
                     <span>{postedAt}</span>
                     <span>{contract}</span>
@@ -36,13 +36,13 @@ const Card = (props) => {
             <hr />
             <div className="flex justify-start items-center flex-wrap gap-2 sm:ml-auto">
                 <button
-                    className="btn py-1 px-2 bg-red-400 rounded-md text-sm font-bold"
+                    className="btn py-1 px-2 bg-[#eef6f6] text-[#5ba4a4] rounded-md text-sm font-bold"
                     value={role}
                     onClick={() => filterFunction("role", role)}>
                     {role}
                 </button>
                 <button
-                    className="btn py-1 px-2 bg-red-400 rounded-md text-sm font-bold"
+                    className="btn py-1 px-2 bg-[#eef6f6] text-[#5ba4a4] rounded-md text-sm font-bold"
                     value={level}
                     onClick={() => filterFunction("level", level)} >
                     {level}
@@ -50,7 +50,7 @@ const Card = (props) => {
                 {languages.map((language, index) => (
                     <button
                         key={index}
-                        className="btn py-1 px-2 bg-red-400 rounded-md text-sm font-bold"
+                        className="btn py-1 px-2 bg-[#eef6f6] text-[#5ba4a4] rounded-md text-sm font-bold"
                         value={language}
                         onClick={() => filterFunction("languages", language)}>
                         {language}
@@ -59,7 +59,7 @@ const Card = (props) => {
                 {tools.map((tool, index) => (
                     <button
                         key={index}
-                        className="btn py-1 px-2 bg-red-400 rounded-md text-sm font-bold"
+                        className="btn py-1 px-2 bg-[#eef6f6] text-[#5ba4a4] rounded-md text-sm font-bold"
                         value={tool}
                         onClick={() => filterFunction("tools", tool)}>
                         {tool}
