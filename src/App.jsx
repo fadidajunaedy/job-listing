c905de0556a59b1dcc9a68b5b89592938f79f3bb
Fadida Zanetti Junaedy <fadidajunaedy@gmail.com>
2023-08-30 07:55:10 +0700

    create clear filter function

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import Card from "./components/Card"
 import Data from "./data.json"
 
@@ -41,22 +41,29 @@ function App() {
     })
   }
 
+  const clearFilter = () => {
+    setArrayFilters([])
+  }
+
   return (
     <>
       <header className="w-full px-8 h-24 bg-[url('/images/bg-header-mobile.svg')] sm:bg-[url('/images/bg-header-desktop.svg')] bg-[#5ba4a4]" >
         <div className="relative h-full mx-auto px-8 max-w-5xl ">
           {arrayFilters.length > 0 &&
-            <div className="absolute inset-x-0 -bottom-8 rounded-md bg-white mx-auto max-w-5xl flex flex-wrap gap-4 bg-base-100 shadow-lg px-8 py-6">
-              {arrayFilters.map((filter, index) => (
-                <span key={index} className="inline-flex items-center rounded-md text-sm font-medium bg-[#eef6f6] text-[#5ba4a4] overflow-hidden">
-                  <span className="py-1 px-2"> {filter.value}</span>
-                  <button type="button" onClick={() => removeFilter(filter)} className="flex-shrink-0 bg-[#5ba4a4] hover:bg-[#2c3a3a] w-7 h-7 p-1">
-                    <svg className="text-white font-bold" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
-                      <path d="M6 18L18 6M6 6l12 12"></path>
-                    </svg>
-                  </button>
-                </span>
-              ))}
+            <div className="absolute inset-x-0 -bottom-8 rounded-md bg-white mx-auto max-w-5xl flex items-center bg-white shadow-lg px-8 py-6">
+              <div className="flex flex-wrap gap-4">
+                {arrayFilters.map((filter, index) => (
+                  <span key={index} className="inline-flex items-center rounded-md text-sm font-medium bg-[#eef6f6] text-[#5ba4a4] overflow-hidden">
+                    <span className="py-1 px-2"> {filter.value}</span>
+                    <button type="button" onClick={() => removeFilter(filter)} className="flex-shrink-0 bg-[#5ba4a4] hover:bg-[#2c3a3a] w-7 h-7 p-1">
+                      <svg className="text-white font-bold" fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" viewBox="0 0 24 24" stroke="currentColor">
+                        <path d="M6 18L18 6M6 6l12 12"></path>
+                      </svg>
+                    </button>
+                  </span>
+                ))}
+              </div>
+              <button className="link ml-auto font-semibold hover:underline hover:underline-offset-2 hover:text-[#5ba4a4]" onClick={clearFilter}>Clear</button>
             </div>
           }
         </div>
